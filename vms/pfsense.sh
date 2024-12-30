a376afe41c33cc02a5104d7d044f7d407ab46ed0
Gabriel Pires <gabriel.pires@deephealth.com>
2024-12-30 12:24:41 +0100

    fix: pfsense create machine script

--- vms/pfsense.sh
+++ vms/pfsense.sh
@@ -7,15 +7,13 @@ qm create $VM_ID \
   --cores 1 \
   --sockets 1 \
   --memory 2048 \
-  --scsihw virtio-scsi-pci
-
-qm set $VM_ID --net0 virtio,bridge=vmbr0
-qm set $VM_ID --net1 virtio,bridge=vmbr1
-qm set $VM_ID --net2 virtio,bridge=vmbr2
-qm set $VM_ID --scsi0 local-lvm:20
-
-qm set $VM_ID --ide2 local:iso/pfSense-CE-2.7.2-RELEASE-amd64.iso
-qm set $VM_ID --boot order=scsi0,ide2
+  --scsihw virtio-scsi-pci \
+  --net0 virtio,bridge=vmbr0 \
+  --net1 virtio,bridge=vmbr1 \
+  --net2 virtio,bridge=vmbr2 \
+  --scsi0 local-lvm:20 \
+  --ide2 local:iso/pfSense-CE-2.7.2-RELEASE-amd64.iso \
+  --boot order=scsi0;ide2
 
 qm config $VM_ID
 qm start $VM_ID

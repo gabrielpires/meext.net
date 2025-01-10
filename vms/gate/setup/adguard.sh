74a60b235113854ece921f0278fd068cbf50dce7
Gabriel Pires <gabriel.pires@deephealth.com>
2025-01-10 18:23:26 +0100

    fix: reunifying vm setup under a single directory

--- vms/gate/setup/adguard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-apt-get install -y curl && \
-curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh -o adguard_install.sh && \
-bash adguard_install.sh
-

7dcc7a9f528f61ace2b60f50f6d3776d589d94d0
Gabriel Pires <gabriel.pires@deephealth.com>
2025-01-16 18:47:07 +0100

    feature: octoprint installation script

---
+++ vms/setup/octoprint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+apt update && \
+apt upgrade -y && \
+apt install -y python3 python3-pip python3-venv python3-dev build-essential && \
+mkdir -p /opt/octoprint && \
+chown $USER:$USER /opt/octoprint && \
+cd /opt/octoprint && \
+python3 -m venv venv && \
+source venv/bin/activate && \
+pip install pip --upgrade && \
+pip install octoprint && \
+cp setup/ocotoprint.service /etc/systemd/system/ && \
+systemctl daemon-reload && \
+systemctl enable octoprint && \
+systemctl start octoprint && \
+systemctl status octoprint
+
+
